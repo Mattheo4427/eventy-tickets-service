21ad26eea35abb4329e9287a9402a15443a5c2e8
AlexisChartier <alexischartier30130pse@gmail.com>
2025-12-07 10:50:00 +0100

    add seed ticket types

---
+++ src/main/resources/db/migration/V2__insert_tickets_types.sql
@@ -0,0 +1,6 @@
+-- 1. Insertion des Types de Billets
+INSERT INTO ticket_type (ticket_type_id, label) VALUES 
+('11111111-1111-1111-1111-111111111111', 'Standard'),
+('22222222-2222-2222-2222-222222222222', 'VIP'),
+('33333333-3333-3333-3333-333333333333', 'Early Bird'),
+('44444444-4444-4444-4444-444444444444', 'Loge');
